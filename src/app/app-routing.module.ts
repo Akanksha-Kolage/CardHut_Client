fb184920eeb7dc760a5096bf1c3d71f3d690755c
Akanksha Kolage <akankshakolage@gmail.com>
2022-07-29 21:18:31 +0530

    CardHut Angular app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,52 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { CardComponent } from './card/card.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
+import { ProductInfoComponent } from './product-info/product-info.component';
+import { ProductsComponent } from './products/products.component';
+import { RegisterComponent } from './register/register.component';
+import { TransactionReceiptComponent } from './transaction-receipt/transaction-receipt.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: '', component: LandingPageComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'dashboard', component: DashboardComponent
+  },
+  {
+    path: 'forgotPassword', component: ForgotpasswordComponent
+  },
+  {
+    path: 'products', component: ProductsComponent
+  },
+  {
+    path: 'productInfo', component: ProductInfoComponent
+  },
+  {
+    path: 'transactions', component: TransactionsComponent
+  },
+  {
+    path: 'transactionReceipt', component: TransactionReceiptComponent
+  },
+  {
+    path: 'card', component: CardComponent
+  },
+  {
+    path: 'adminLogin', component: AdminLoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { TransactionReceiptComponent } from './transaction-receipt/transaction-receipt.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductInfoComponent } from './product-info/product-info.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CardComponent } from './card/card.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ProductsComponent,
+    ForgotpasswordComponent,
+    TransactionsComponent,
+    TransactionReceiptComponent,
+    LandingPageComponent,
+    DashboardComponent,
+    ProductInfoComponent,
+    CardComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
